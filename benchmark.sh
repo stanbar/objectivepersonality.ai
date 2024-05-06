fe008a9cfe8a5aa0ed86f8c9d9a4258f267386c5
Stanislaw Baranski <stan@stan.bar>
2024-05-06 19:44:36 +0200

    dont force poetry

--- benchmark.sh
+++ benchmark.sh
@@ -3,10 +3,10 @@
 echo "Running benchmarks..."
 
 echo "---------Centroids----------"
-poetry run python objectivepersonality_ai/benchmarks/centroids.py
+python objectivepersonality_ai/benchmarks/centroids.py
 
 echo "---------Neural-Network----------"
-poetry run python objectivepersonality_ai/benchmarks/neural_network.py
+python objectivepersonality_ai/benchmarks/neural_network.py
 
 echo "---------Proto-Networks----------"
-poetry run python objectivepersonality_ai/benchmarks/proto_networks.py
+python objectivepersonality_ai/benchmarks/proto_networks.py
