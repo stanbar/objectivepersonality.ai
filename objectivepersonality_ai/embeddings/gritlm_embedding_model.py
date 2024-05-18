05a8398b659155b992b4696e3222c4a926d8f09e
Stanislaw Baranski <stan@stan.bar>
2024-05-18 18:27:23 +0200

    update embedding creation

--- objectivepersonality_ai/embeddings/gritlm_embedding_model.py
+++ objectivepersonality_ai/embeddings/gritlm_embedding_model.py
@@ -4,17 +4,19 @@ from .embeddings_model import EmbeddingModel
 
 
 class GritLMEmbeddings(EmbeddingModel):
-    def __init__(self, instruction: str = None) -> None:
-        self.model_name = "GritLM/GritLM-7B"
-        self.model = GritLM(self.model_name, mode="embedding", torch_dtype="auto")
-        self.instruction = self.gritlm_instruction(instruction)
 
+    @staticmethod
     def gritlm_instruction(instruction):
         return (
             "<|user|>\n" + instruction + "\n<|embed|>\n"
             if instruction
             else "<|embed|>\n"
         )
+
+    def __init__(self, instruction: str = None) -> None:
+        self.model_name = "GritLM/GritLM-7B"
+        self.model = GritLM(self.model_name, mode="embedding", torch_dtype="auto")
+        self.instruction = self.gritlm_instruction(instruction)
 
     def _embed(self, sentence):
         return self.model.encode(sentence, instruction=self.instruction)
