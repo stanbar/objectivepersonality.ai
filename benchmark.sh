a853d81d5e397002a122ca6505158e9a7a9c1f6a
Stanislaw Baranski <stan@stan.bar>
2024-06-12 11:47:42 +0200

    reorder benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -8,11 +8,11 @@ python3 benchmark_centroids.py
 echo "---------Weighted-Centroids----------"
 python3 benchmark_weighted_centroids.py
 
+echo "---------Random-Forest----------"
+python3 benchmark_random_forest.py
+
 echo "---------Neural-Network----------"
 python3 benchmark_neural_network.py
 
 echo "---------Proto-Networks----------"
 python3 benchmark_proto_networks.py
-
-echo "---------Random-Forest----------"
-python3 benchmark_random_forest.py
