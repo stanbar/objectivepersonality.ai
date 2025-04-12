d9f1fdde80f034afc503f16be1b862bf76416263
Stanislaw Baranski <stan@stan.bar>
2025-04-12 15:04:24 +0200

    feat: refactor ops code constants and methods

--- objectivepersonality_ai/ops.py
+++ objectivepersonality_ai/ops.py
@@ -23,10 +23,10 @@ MASCULINE = "M"
 FEMININE = "F"
 OBSERVER = "Observer"
 DECIDER = "Decider"
-Oi = "Oi"
-Oe = "Oe"
-Di = "Di"
-De = "De"
+OI = "Oi"
+OE = "Oe"
+DI = "Di"
+DE = "De"
 THINKING = "T"
 FEELING = "F"
 INTUITION = "N"
@@ -48,6 +48,7 @@ PATTERN = re.compile(
     r"([FMx])([FMx])-([SNFTx][iex])\/([SNFTx][iex])-([SPBCx])([SPBCx])\/([SPBCx])\(([SPBCx])\)(?: \[(\d)\])?"
 )
 
+
 @dataclass
 class OpProfile:
     mS: Optional[str]
@@ -66,84 +67,177 @@ class OpProfile:
     Quadrant: Optional[str]
     MBTI: Optional[str]
 
+    def is_observer(self) -> bool:
+        return self.ObserverDecider == OBSERVER
+
+    def is_decider(self) -> bool:
+        return self.ObserverDecider == DECIDER
+
+    def is_di(self) -> bool:
+        return self.DiDe == DI
+
+    def is_de(self) -> bool:
+        return self.DiDe == DE
+
+    def is_oi(self) -> bool:
+        return self.OiOe == OI
+
+    def is_oe(self) -> bool:
+        return self.OiOe == OE
+
+    def is_sensing(self) -> bool:
+        return self.NS == SENSING
+
+    def is_intuition(self) -> bool:
+        return self.NS == INTUITION
+
+    def is_thinking(self) -> bool:
+        return self.FT == THINKING
+
+    def is_feeling(self) -> bool:
+        return self.FT == FEELING
+
+    def is_sleep(self) -> bool:
+        return self.SleepPlay == SLEEP
+
+    def is_play(self) -> bool:
+        return self.SleepPlay == PLAY
+
+    def is_consume(self) -> bool:
+        return self.ConsumeBlast == CONSUME
+
+    def is_blast(self) -> bool:
+        return self.ConsumeBlast == BLAST
+
+    def is_info(self) -> bool:
+        return self.InfoEnergy == INFO
+
+    def is_energy(self) -> bool:
+        return self.InfoEnergy == ENERGY
+
+    def is_intro(self) -> bool:
+        return self.IntroExtro == INTRO
+
+    def is_extro(self) -> bool:
+        return self.IntroExtro == EXTRO
+
+    def is_flex(self) -> bool:
+        return self.FlexFriends == FLEX
+
+    def is_friends(self) -> bool:
+        return self.FlexFriends == FRIENDS
+
+    def is_spec(self) -> bool:
+        return self.GenSpec == SPECIALISATION
+
+    def is_gen(self) -> bool:
+        return self.GenSpec == GENERALISATION
+
+
 # Utility functions
 def is_observer(value: str) -> bool:
     return value in ["Si", "Se", "Ni", "Ne", "Oe", "Oi", "Sx", "Nx"]
 
+
 def is_decider(value: str) -> bool:
     return value in ["Ti", "Te", "Fi", "Fe", "De", "Di", "Tx", "Fx"]
+
 
 def is_di(value: str) -> bool:
     return value in ["Ti", "Fi", "Di"]
 
+
 def is_de(value: str) -> bool:
     return value in ["Te", "Fe", "De"]
+
 
 def is_oi(value: str) -> bool:
     return value in ["Si", "Ni", "Oi"]
 
+
 def is_oe(value: str) -> bool:
     return value in ["Se", "Ne", "Oe"]
+
 
 def is_sensing(value: str) -> bool:
     return value in ["Si", "Se", "Sx"]
 
+
 def is_intuition(value: str) -> bool:
     return value in ["Ni", "Ne", "Nx"]
+
 
 def is_thinking(value: str) -> bool:
     return value in ["Ti", "Te", "Tx"]
 
+
 def is_feeling(value: str) -> bool:
     return value in ["Fi", "Fe", "Fx"]
+
 
 def is_sleep(value: str) -> bool:
     return value == "S"
 
+
 def is_play(value: str) -> bool:
     return value == "P"
+
 
 def is_consume(value: str) -> bool:
     return value == "C"
 
+
 def is_blast(value: str) -> bool:
     return value == "B"
+
 
 def is_info(value: str) -> bool:
     return value in ["S", "P"]
 
+
 def is_energy(value: str) -> bool:
     return value in ["C", "B"]
+
 
 def is_intro(value: str) -> bool:
     return value in ["P", "B"]
 
+
 def is_extro(value: str) -> bool:
     return value in ["S", "C"]
+
 
 def is_flex(value: str) -> bool:
     return value in ["1", "3"]
 
+
 def is_friends(value: str) -> bool:
     return value in ["2", "4"]
+
 
 def is_spec(value: str) -> bool:
     return value in ["3", "4"]
 
+
 def is_gen(value: str) -> bool:
     return value in ["1", "2"]
+
 
 def is_se_ni(func1: str, func2: str) -> bool:
     return func1 in ["Se", "Ni"] or func2 in ["Se", "Ni"]
 
+
 def is_ne_si(func1: str, func2: str) -> bool:
     return func1 in ["Si", "Ne"] or func2 in ["Si", "Ne"]
+
 
 def is_te_fi(func1: str, func2: str) -> bool:
     return func1 in ["Te", "Fi"] or func2 in ["Te", "Fi"]
 
+
 def is_fe_ti(func1: str, func2: str) -> bool:
     return func1 in ["Fe", "Ti"] or func2 in ["Fe", "Ti"]
+
 
 def get_quadrant(func1: str, func2: str) -> Optional[str]:
     if is_se_ni(func1, func2) and is_te_fi(func1, func2):
@@ -155,6 +249,7 @@ def get_quadrant(func1: str, func2: str) -> Optional[str]:
     if is_ne_si(func1, func2) and is_fe_ti(func1, func2):
         return "Alpha"
     return None
+
 
 def get_mbti(func1: str, func2: str) -> Optional[str]:
     if func1 == "Se" and func2 in ["Ti", "Fe"]:
@@ -191,31 +286,88 @@ def get_mbti(func1: str, func2: str) -> Optional[str]:
         return "INFP"
     return None
 
+
 def decode_op_code(ops: str) -> OpProfile:
     """
     Decode an op-code string and return an OpProfile dataclass.
     """
     match = PATTERN.search(ops)
     if match:
-        mS_val, mDe_val, func1, func2, animal1, animal2, animal3, animal4, social = match.groups()[:9]
+        mS_val, mDe_val, func1, func2, animal1, animal2, animal3, animal4, social = (
+            match.groups()[:9]
+        )
 
-        def get_coin_value(check1: bool, check2: bool, value1: str, value2: str) -> Optional[str]:
+        def get_coin_value(
+            check1: bool, check2: bool, value1: str, value2: str
+        ) -> Optional[str]:
             return value1 if check1 else value2 if check2 else None
 
         op_profile = OpProfile(
-            mS=get_coin_value(mS_val == MASCULINE, mS_val == FEMININE, MASCULINE, FEMININE) or None,
-            mDe=get_coin_value(mDe_val == MASCULINE, mDe_val == FEMININE, MASCULINE, FEMININE) or None,
-            ObserverDecider=OBSERVER if is_observer(func1) else DECIDER if is_decider(func1) else None,
-            DiDe=get_coin_value(is_di(func1) or is_di(func2), is_de(func1) or is_de(func2), Di, De) or None,
-            OiOe=get_coin_value(is_oi(func1) or is_oi(func2), is_oe(func1) or is_oe(func2), Oi, Oe) or None,
-            NS=get_coin_value(is_sensing(func1) or is_sensing(func2), is_intuition(func1) or is_intuition(func2), SENSING, INTUITION) or None,
-            FT=get_coin_value(is_thinking(func1) or is_thinking(func2), is_feeling(func1) or is_feeling(func2), THINKING, FEELING) or None,
-            SleepPlay=get_coin_value(is_sleep(animal1) or is_sleep(animal2), is_play(animal1) or is_play(animal2), SLEEP, PLAY) or None,
-            ConsumeBlast=get_coin_value(is_consume(animal1) or is_consume(animal2), is_blast(animal1) or is_blast(animal2), CONSUME, BLAST) or None,
-            InfoEnergy=get_coin_value(is_info(animal4), is_energy(animal4), INFO, ENERGY) or None,
-            IntroExtro=get_coin_value(is_intro(animal4), is_extro(animal4), INTRO, EXTRO) or None,
-            FlexFriends=get_coin_value(is_flex(social), is_friends(social), FLEX, FRIENDS) or None,
-            GenSpec=get_coin_value(is_spec(social), is_gen(social), SPECIALISATION, GENERALISATION) or None,
+            mS=get_coin_value(
+                mS_val == MASCULINE, mS_val == FEMININE, MASCULINE, FEMININE
+            )
+            or None,
+            mDe=get_coin_value(
+                mDe_val == MASCULINE, mDe_val == FEMININE, MASCULINE, FEMININE
+            )
+            or None,
+            ObserverDecider=(
+                OBSERVER
+                if is_observer(func1)
+                else DECIDER if is_decider(func1) else None
+            ),
+            DiDe=get_coin_value(
+                is_di(func1) or is_di(func2), is_de(func1) or is_de(func2), DI, DE
+            )
+            or None,
+            OiOe=get_coin_value(
+                is_oi(func1) or is_oi(func2), is_oe(func1) or is_oe(func2), OI, OE
+            )
+            or None,
+            NS=get_coin_value(
+                is_sensing(func1) or is_sensing(func2),
+                is_intuition(func1) or is_intuition(func2),
+                SENSING,
+                INTUITION,
+            )
+            or None,
+            FT=get_coin_value(
+                is_thinking(func1) or is_thinking(func2),
+                is_feeling(func1) or is_feeling(func2),
+                THINKING,
+                FEELING,
+            )
+            or None,
+            SleepPlay=get_coin_value(
+                is_sleep(animal1) or is_sleep(animal2),
+                is_play(animal1) or is_play(animal2),
+                SLEEP,
+                PLAY,
+            )
+            or None,
+            ConsumeBlast=get_coin_value(
+                is_consume(animal1) or is_consume(animal2),
+                is_blast(animal1) or is_blast(animal2),
+                CONSUME,
+                BLAST,
+            )
+            or None,
+            InfoEnergy=get_coin_value(
+                is_info(animal4), is_energy(animal4), INFO, ENERGY
+            )
+            or None,
+            IntroExtro=get_coin_value(
+                is_intro(animal4), is_extro(animal4), INTRO, EXTRO
+            )
+            or None,
+            FlexFriends=get_coin_value(
+                is_flex(social), is_friends(social), FLEX, FRIENDS
+            )
+            or None,
+            GenSpec=get_coin_value(
+                is_spec(social), is_gen(social), SPECIALISATION, GENERALISATION
+            )
+            or None,
             Quadrant=get_quadrant(func1, func2) or None,
             MBTI=get_mbti(func1, func2) or None,
         )
@@ -224,22 +376,12 @@ def decode_op_code(ops: str) -> OpProfile:
         raise ValueError(f"Unable to parse ops string: {ops}")
 
 
-
-
-
-
-
-
-
-
-
-
 COINS_DICT = {
     mSCoin: (FEMININE, MASCULINE),
     mDeCoin: (FEMININE, MASCULINE),
     ObserverDeciderCoin: (OBSERVER, DECIDER),
-    DiDeCoin: (Di, De),
-    OiOeCoin: (Oi, Oe),
+    DiDeCoin: (DI, DE),
+    OiOeCoin: (OI, OE),
     NSCoin: (INTUITION, SENSING),
     FTCoin: (FEELING, THINKING),
     SleepPlayCoin: (SLEEP, PLAY),
@@ -258,6 +400,7 @@ COINS_AUXILIARY = {
     "ObserverAxis": ("Se/Ni", "Ne/Si"),
     "DeciderAxis": ("Te/Fi", "Fe/Ti"),
 }
+
 
 def format_coins(coins):
     return f"""Modalities: {coins[mSCoin]}{coins[mDeCoin]}
